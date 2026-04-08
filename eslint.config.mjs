fa0ab5214bf94b595f6fd9930ae27b21df9e9e12
Barry Forrest <barry.forrest@gmail.com>
2026-04-08 08:02:31 -0500

    new linter, updated The List to be an index page for `Good To Know` Issues

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import mdx from 'eslint-plugin-mdx';
+
+export default [
+  {
+    ...mdx.flat,
+    processor: mdx.createRemarkProcessor({
+      lintCodeBlocks: false,
+    }),
+  },
+  {
+    ...mdx.flatCodeBlocks,
+    rules: {
+      ...mdx.flatCodeBlocks.rules,
+    },
+  },
+];
